f77b46ab170f2fd8bb8c04ef21fa3ad1db766b6b
Rice_Field <famima65536@gmail.com>
2017-10-22 18:34:29 +0900

    型の指定

--- src/RuinPray/ui/elements/Input.php
+++ src/RuinPray/ui/elements/Input.php
@@ -8,17 +8,17 @@ class Input extends BaseElement {
   public $placeholder = null;
   public $default = null;
 
-  public function placeholder(string $placeholder) {
+  public function placeholder(string $placeholder) : Input {
     $this->placeholder = $placeholder;
     return $this;
   }
 
-  public function default(string $default) {
+  public function default(string $default) : Input {
     $this->default = $default;
     return $this;
   }
 
-  public function content() {
+  public function content() : array {
     return [
       "type" => self::TYPE,
       "text" => $this->text,
